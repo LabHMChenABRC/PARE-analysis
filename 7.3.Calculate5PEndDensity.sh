edd011540c279b9781bc6fe3ed7f80a4b64c2775
LabHMChenABRC <58059039+LabHMChenABRC@users.noreply.github.com>
2024-03-30 16:20:56 +0800

    Update 7.3.Calculate5PEndDensity.sh

--- 7.3.Calculate5PEndDensity.sh
+++ 7.3.Calculate5PEndDensity.sh
@@ -2,4 +2,4 @@
 wd=$PWD
 BedDir=$wd/PARE/alignments/bed
 gtfFile=$wd/Reference_sequence/TAIR10_representative_gene_models.gtf
-Rscript $wd/GuitarPlotFast.R -b $BedDir -g $gtfFile -o $wd/PARE/metagenePlot
+GuitarPlotFast.R -b $BedDir -g $gtfFile -o $wd/PARE/metagenePlot
